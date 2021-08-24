8f2f242897b31fcffdf750f4554a0f32d8d1ea63
dafex301 <lfahrel@gmail.com>
2021-08-24 17:04:55 +0700

    Making hero and adding json

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import Navbar from 'components/Navbar'
+import Hero from 'components/Hero'
+
+import landingPage from 'json/landingPage.json' 
+
 
 export default class LandingPage extends Component {
     render() {
         return (
             <>
-                <Navbar {...this.props}></Navbar>  
+                <Navbar {...this.props}></Navbar>
+                <Hero data={landingPage.hero} />
             </>
         )
     }
