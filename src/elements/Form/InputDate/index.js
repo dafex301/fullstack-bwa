a6fe57ecb58f46d84b5ea7635dd58361c2eb7487
dafex301 <lfahrel@gmail.com>
2021-09-14 00:43:55 +0700

    continuing inputDate

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -32,10 +32,55 @@ export default function Date(props) {
 		};
 	});
 
-	// TODO
 	const refDate = useRef(null);
+	const handleClickOutside = (event) => {
+		if (refDate && !refDate.current.contains(event.target)) {
+			setIsShowed(false);
+		}
+	};
 
-	return <div></div>;
+	const check = (focus) => {
+		focus.indexOf(1) < 0 && setIsShowed(false);
+	};
+
+	const displayDate = `${value.startDate ? formatDate(value.startDate) : ''}${
+		value.endDate ? ' - ' + formatDate(value.endDate) : ''
+	}`;
+
+	return (
+		<div
+			ref={refDate}
+			className={['input-date mb-3', props.outerClassName].join(' ')}
+		>
+			<div className="input-group">
+				<div className="input-group-prepend bg-gray-900">
+					<span className="input-group-text">
+						<img src={iconCalendar} alt="icon calendar" />
+					</span>
+				</div>
+				<input
+					readOnly
+					type="text"
+					className="form-control"
+					value={displayDate}
+					placeholder={placeholder}
+					onClick={() => setIsShowed(!isShowed)}
+				/>
+
+				{isShowed && (
+					<div className="date-range-wrapper">
+						<DateRange
+							editableDateInputs={true}
+							onChange={datePickerChange}
+							moveRangeOnFirstSelection={false}
+							onRangeFocusChange={check}
+							ranges={[value]}
+						/>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 Date.propTypes = {
