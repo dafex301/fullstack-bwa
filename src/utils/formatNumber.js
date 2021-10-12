c01f4b1744ae684c61edc0d1a36362ce9ebce5a4
dafex301 <lfahrel@gmail.com>
2021-10-12 22:51:35 +0700

    testing input date + utils formatDate

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,5 +1,10 @@
-const formatNumber = (number) => {
-  return number.toLocaleString();
-};
+// const formatNumber = (number) => {
+//   return number.toLocaleString();
+// };
 
-export default formatNumber;
+// export default formatNumber;
+
+export default (number) => {
+	const formatNumbering = new Intl.NumberFormat('id-ID');
+	return formatNumbering.format(number);
+};
