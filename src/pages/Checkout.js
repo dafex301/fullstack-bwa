891a22999ea3a6e779bbae3d2cb80bf9e1a8650f
dafex301 <lfahrel@gmail.com>
2021-10-15 22:43:16 +0700

    Checkout Page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+
 import Navbar from 'components/Navbar';
+import Button from 'elements/Button';
+import Stepper, {
+	Numbering,
+	Meta,
+	MainContent,
+	Controller,
+} from 'elements/Stepper';
+
+import BookingInformation from 'components/Checkout/BookingInformation';
+import Payment from 'components/Checkout/Payment';
+import Completed from 'components/Checkout/Completed';
+
+import ItemDetails from 'json/itemDetails.json';
 
 export default class Checkout extends Component {
 	render() {
